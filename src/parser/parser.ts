93d3506f089335ef3972c8a71fe3e447378b95bf
Eric Rovell <ericrovell@gmail.com>
2021-07-27 17:58:35 +0300

    add: rational number parser

---
+++ src/parser/parser.ts
@@ -0,0 +1,36 @@
+import { 
+  parseIntegers,
+  parseFloat,
+  parseArray,
+  parseObject
+} from "./parsers";
+
+import { handleRatioSign } from "@util/helpers";
+import type { Input, Ratio } from "@types";
+
+const parsers = [
+  parseIntegers,
+  parseFloat,
+  parseArray,
+  parseObject
+];
+
+/**
+ * Parses input into Ratio.
+ * 
+ * Supported input:
+ *  + parse(int, int);
+ *  + parse(float);
+ *  + parse([ int, int ]);
+ *  + parse({ n: int, d: int });
+ */
+export function parse(numerator: Input, denominator: number | undefined): Ratio | null {
+  for (const parser of parsers) {
+    const parsed = parser(numerator, denominator);
+    if (parsed) {
+      return handleRatioSign(parsed);
+    }
+  }
+
+  return null;
+}
