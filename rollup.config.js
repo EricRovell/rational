bd723c24c904be007b76ecc8c0810bcc5d4273eb
Eric Rovell <ericrovell@gmail.com>
2021-07-28 16:20:33 +0300

    add: bundler config

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import typescript from "rollup-plugin-typescript2";
+import { terser } from "rollup-plugin-terser";
+
+/**
+ * Generates Rollup Plugin Configuration.
+ */
+function getPluginsConfig(compilerOptions) {
+  return [
+    typescript({
+      tsconfigOverride: { compilerOptions }
+    }),
+    terser({
+      ecma: 2018,
+      module: true,
+      toplevel: true,
+      compress: { pure_getters: true },
+      format: { wrap_func_args: false }
+    })
+  ];
+}
+
+/**
+ * Bundle everything into ESM module.
+ */
+export default [
+  /**
+   * Core bundle build in ESM
+   */
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.js",
+      format: "es"
+    },
+    plugins: getPluginsConfig({ declaration: true })
+  }
+];
