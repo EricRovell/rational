6632e402f85ff4d10e17c1c2e66726c26d666e6b
Eric Rovell <ericrovell@gmail.com>
2021-08-12 23:23:16 +0300

    fix: lint error

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -3,7 +3,7 @@ import { Ratio } from "@types";
 /**
  * Round the number up to the desired precision.
  */
- export function round(number: number, digits = 0, base = Math.pow(10, digits)): number {
+export function round(number: number, digits = 0, base = Math.pow(10, digits)): number {
 	return Math.round(number * base) / base + 0;
 }
 
