6dafd03c507bc010f23e4d1571a612c161fb9f32
Eric Rovell <ericrovell@gmail.com>
2021-08-22 14:42:58 +0300

    fix: edge cases and types errors

--- src/util/repeating-decimal.ts
+++ src/util/repeating-decimal.ts
@@ -4,13 +4,13 @@ import type { Ratio, RepeatingDecimal } from "../types";
 /**
  * Produces a ratio from repeating decimal object.
  */
-export function getRatioFromRepeatingDecimal({ sign = 1, int = 0, ...fractional }: RepeatingDecimal): Ratio | null {
+export function getRatioFromRepeatingDecimal({ sign = 1, int = 0, nonrepeat = "", repeat }: RepeatingDecimal): Ratio | null {
 	const integralPart = Number(int);
-	const nonrepeat = fractional.nonrepeat ? String(fractional.nonrepeat) : "";
-	const repeat = fractional.repeat ? String(fractional.repeat) : "";
+	const nonperiod = String(nonrepeat) || "";
+	const period = String(repeat);
 
-	const denominator = Number(`${"9".repeat(repeat.length)}${"0".repeat(nonrepeat.length)}`); 
-	const numerator =  Number(`${nonrepeat}${repeat}`) - Number(nonrepeat);
+	const denominator = Number(`${"9".repeat(period.length)}${"0".repeat(nonperiod.length)}`); 
+	const numerator =  Number(`${nonperiod}${repeat}`) - Number(nonperiod);
 
 	return getRatio(
 		sign * (integralPart * denominator + numerator),
